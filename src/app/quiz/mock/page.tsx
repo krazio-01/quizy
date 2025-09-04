d81988974e956edf5e4356bda1f879d8a9f7e6be
Mohd Amman <md.krazio@gmail.com>
2025-09-04 22:42:03 +0530

    stored selected grade in sessionstorage

--- src/app/quiz/mock/page.tsx
+++ src/app/quiz/mock/page.tsx
@@ -11,16 +11,16 @@ const Quiz = () => {
     const router = useRouter();
 
     useEffect(() => {
-        // const grade = sessionStorage.getItem('selectedGrade');
-        if (!selectedGrade?.trim()) {
+        const grade = sessionStorage.getItem('selectedGrade');
+        if (!grade?.trim()) {
             router.replace('/');
             return;
         }
 
-        const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-            e.preventDefault();
-            e.returnValue = '';
-        };
+        // const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+        //     e.preventDefault();
+        //     e.returnValue = '';
+        // };
 
         // window.addEventListener('beforeunload', handleBeforeUnload);
         // return () => window.removeEventListener('beforeunload', handleBeforeUnload);
--- src/app/quiz/mock/rules/page.tsx
+++ src/app/quiz/mock/rules/page.tsx
@@ -12,16 +12,16 @@ const Page = () => {
     const router = useRouter();
 
     useEffect(() => {
-        // const grade = sessionStorage.getItem('selectedGrade');
-        if (!selectedGrade?.trim()) {
+        const grade = sessionStorage.getItem('selectedGrade');
+        if (!grade?.trim()) {
             router.replace('/');
             return;
         }
 
-        const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-            e.preventDefault();
-            e.returnValue = '';
-        };
+        // const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+        //     e.preventDefault();
+        //     e.returnValue = '';
+        // };
 
         // window.addEventListener('beforeunload', handleBeforeUnload);
         // return () => window.removeEventListener('beforeunload', handleBeforeUnload);
