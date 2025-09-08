a10f1141eb327dd0b63461b008c1be1b137779e5
Mohd Amman <md.krazio@gmail.com>
2025-09-08 22:01:49 +0530

    change base fare

--- src/app/api/payment/initiate/route.ts
+++ src/app/api/payment/initiate/route.ts
@@ -4,7 +4,7 @@ import UserModel from '@/models/UserModel';
 import PaymentModel from '@/models/PayMentModel';
 import axios from 'axios';
 
-const BASE_AMOUNT = 75;
+const BASE_AMOUNT = 5;
 const BASE_CURRENCY = 'AED';
 
 const COMMON_CURRENCIES = new Set([
