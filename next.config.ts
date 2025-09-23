6bf2634c935acd083ffec3a33870114c60d71705
Mohd Amman <md.krazio@gmail.com>
2025-09-23 11:49:40 +0530

    fix ip-address bug

--- next.config.ts
+++ next.config.ts
@@ -35,22 +35,22 @@ const nextConfig: NextConfig = {
                     {
                         key: 'Content-Security-Policy',
                         value: `
-                          default-src 'self';
-                          script-src 'self' 'unsafe-inline' 'unsafe-eval'
-                            https://www.google.com https://www.gstatic.com
-                            https://www.google-analytics.com https://www.googletagmanager.com
-                            https://recaptcha.google.com https://www.recaptcha.net
-                            https://js.payglocal.com https://*.stripe.com;
-                          style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-                          img-src 'self' data: blob: https://res.cloudinary.com https://www.google.com https://www.gstatic.com;
-                          font-src 'self' https://fonts.gstatic.com;
-                          frame-src 'self' https://www.youtube.com https://www.google.com https://recaptcha.google.com https://www.recaptcha.net;
-                          connect-src 'self' https://res.cloudinary.com https://*.google.com https://*.googleapis.com https://www.google-analytics.com https://js.payglocal.com https://*.stripe.com;
-                          object-src 'none';
-                          base-uri 'self';
-                          form-action 'self';
-                          frame-ancestors 'self';
-                        `
+                            default-src 'self';
+                            script-src 'self' 'unsafe-inline' 'unsafe-eval'
+                                https://www.google.com https://www.gstatic.com
+                                https://www.google-analytics.com https://www.googletagmanager.com
+                                https://recaptcha.google.com https://www.recaptcha.net
+                                https://js.payglocal.com;
+                            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                            img-src 'self' data: blob: https://res.cloudinary.com https://www.google.com https://www.gstatic.com;
+                            font-src 'self' https://fonts.gstatic.com;
+                            frame-src 'self' https://www.youtube.com https://www.google.com https://recaptcha.google.com https://www.recaptcha.net;
+                            connect-src 'self' https://res.cloudinary.com https://*.google.com https://*.googleapis.com https://www.google-analytics.com https://js.payglocal.com https://ipapi.co;
+                            object-src 'none';
+                            base-uri 'self';
+                            form-action 'self';
+                            frame-ancestors 'self';
+                            `
                             .replace(/\s{2,}/g, ' ')
                             .trim(),
                     },
